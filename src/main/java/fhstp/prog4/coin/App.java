1a2bbabd076197b5ed4ab7a4026df36699240a07
Stefan Berger <is191308@fhstp.ac.at>
2021-11-27 14:44:04 +0100

    Push Hell2

--- src/main/java/fhstp/prog4/coin/App.java
+++ src/main/java/fhstp/prog4/coin/App.java
@@ -3,7 +3,7 @@ package fhstp.prog4.coin;
 public class App {
     
     public static void main(String[] args) {
-    	System.out.println("Hallo");
+    	System.out.println("Hallo2");
     }
     
 }
