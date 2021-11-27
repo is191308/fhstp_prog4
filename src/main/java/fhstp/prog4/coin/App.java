9428fab17093289420923399d36dddbbc590052e
Stefan <Stefan@Stefan-PC.mshome.net>
2021-11-27 13:17:53 +0100

    Initial

---
+++ src/main/java/fhstp/prog4/coin/App.java
@@ -0,0 +1,8 @@
+package fhstp.prog4.coin;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/test/java/fhstp/prog4/coin/AppTest.java
@@ -0,0 +1,13 @@
+package fhstp.prog4.coin;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
