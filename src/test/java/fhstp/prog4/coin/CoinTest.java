c20b87f1a91135e0c078fd9344437385ef450ced
Stefan Berger <is191308@fhstp.ac.at>
2021-11-28 15:49:22 +0100

    static import

--- src/test/java/fhstp/prog4/coin/CoinTest.java
+++ src/test/java/fhstp/prog4/coin/CoinTest.java
@@ -1,14 +1,11 @@
 package fhstp.prog4.coin;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.lang.reflect.Executable;
-
 import org.junit.jupiter.api.Test;
 
 import fhstp.prog4.coin.Coin.CoinBuilder;
 
-import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.MatcherAssert.*; 
 import static org.hamcrest.Matchers.*;
 
 public class CoinTest {
