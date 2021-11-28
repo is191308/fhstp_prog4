fedd4d1511fd83fb96156a4f8c98c8bf9b477754
Stefan Berger <is191308@fhstp.ac.at>
2021-11-28 14:41:15 +0100

    First Prototype

--- src/main/java/fhstp/prog4/coin/App.java
+++ src/main/java/fhstp/prog4/coin/App.java
@@ -3,7 +3,15 @@ package fhstp.prog4.coin;
 public class App {
     
     public static void main(String[] args) {
-    	System.out.println("NEW2");
+    	CoinList cl = new CoinList("ed40901f-9d05-4879-a2a7-b29fd7047d02");
+    	if (cl.refreshCoinList()) {
+    		cl.getCoinList().stream()
+			.sorted((c1, c2) -> Double.compare(c2.getPriceUSD(), c1.getPriceUSD()))
+			.forEach(c1 -> System.out.println(c1));
+    	} else {
+    		System.out.println("Failed to refresh CoinList!");
+    	}
+    	
     }
     
 }
