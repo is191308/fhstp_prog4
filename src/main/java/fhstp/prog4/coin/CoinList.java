dce8f8e66c9ce3552d79ada2ca392aa730041a1c
Stefan Berger <is191308@fhstp.ac.at>
2021-11-28 14:42:02 +0100

    First Prototype

--- src/main/java/fhstp/prog4/coin/CoinList.java
+++ src/main/java/fhstp/prog4/coin/CoinList.java
@@ -1,11 +1,7 @@
 package fhstp.prog4.coin;
 
+import fhstp.prog4.coin.Coin.CoinBuilder;
 
-import java.io.IOException;
-//import java.net.Authenticator;
-//import java.net.HttpRetryException;
-//import java.net.InetSocketAddress;
-//import java.net.ProxySelector;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Redirect;
@@ -14,63 +10,73 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
-//import java.util.Map;
-//import java.util.HashMap;
-//import java.util.stream.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.io.IOException;
+import org.json.*;
 
 public class CoinList {
+	public static final String apiBaseURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency";
+	private String apiKey;
+	private Set<Coin> coinList;
 
-	
-   public static void test() {
-
-		final String apiKey = "31a76587-bd71-4b4c-968c-d8641a46b85f";
-		String uri =  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
-		
-	
-		//Map<String, String>  authHeader = new HashMap<>();
-		//authHeader.put("X-CMC_PRO_API_KEY", apiKey);
-	
-		try {
-			HttpClient client = HttpClient.newBuilder()
-				.version(Version.HTTP_1_1)
-				.followRedirects(Redirect.NORMAL)
-				.connectTimeout(Duration.ofSeconds(10))
-				//.proxy(ProxySelector.of(new InetSocketAddress("proxy.example.com", 80)))
-				//.authenticator(Authenticator.getDefault())
-				.build();
-			HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(uri))
-				.header("X-CMC_PRO_API_KEY", apiKey)
-				.build();	
-	
-			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-			System.out.println(response.statusCode());
-			System.out.println(response.body());
-		
-		
+	public CoinList(String apiKey) {
+		if (apiKey == null) {
+			throw new IllegalArgumentException("apiKey: null not allowed");
 		}
-		catch ( IOException | InterruptedException e ){
-			System.err.println("HTTP Exception: "+ e.getMessage());
-			e.printStackTrace();
-		
+		this.apiKey = apiKey;
+	}
+
+	public Set<Coin> getCoinList() {
+		return this.coinList;
+	}
+	
+	
+	public boolean refreshCoinList() {
+		final String uri = apiBaseURL + "/listings/latest";
+
+		try {
+			HttpClient client = HttpClient.newBuilder().version(Version.HTTP_1_1).followRedirects(Redirect.NORMAL)
+					.connectTimeout(Duration.ofSeconds(10)).build();
+			HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uri)).header("X-CMC_PRO_API_KEY", this.apiKey)
+					.build();
+
+			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+			if (response.statusCode() != 200) {
+				return false;
 			}
+
+			this.coinList = convertJsonToCoin(response.body());
+			return true;
+		} catch (IOException | InterruptedException e) {
+			System.err.println("Failed to retrive data from " + uri + ": " + e.getMessage());
+			return false;
 		}
 	}
 
-	
-	/*
-	    catch(IOException er) {
-	    	System.err.println("IOException: "+ er.getMessage());
-	    	e.printStackTrace();
-	    	
-	    } catch (InterruptedException err) {
-	    	System.err.println("InterruptedException:: "+ err.getMessage());
-			e.printStackTrace();
+	private Set<Coin> convertJsonToCoin(String jsonString) {
+		Set<Coin> coinl = new HashSet<Coin>();
+		JSONArray coins = null;
+		try {
+			JSONObject jObj = new JSONObject(jsonString);
+			coins = jObj.getJSONArray("data");
+		} catch (JSONException jex) {
+			throw new IllegalArgumentException("failed to parse string as json");
 		}
-	    */  
-			
-		   // finally {
-		    //	client.releaseConnection();
-		    //}
-
-
+		for (Object o : coins) {
+			if (o instanceof JSONObject) {
+				try {
+					Coin c = new CoinBuilder(((JSONObject) o).getString("name"), ((JSONObject) o).getString("symbol"))
+							.priceUSD(((JSONObject) o).getJSONObject("quote").getJSONObject("USD").getDouble("price"))
+							.percentChange24(((JSONObject) o).getJSONObject("quote").getJSONObject("USD").getDouble("percent_change_24h"))
+							.build();
+					coinl.add(c);
+				} catch (JSONException | IllegalArgumentException jex) {
+					jex.printStackTrace();
+					System.out.println("Skip invalid object!");
+				}
+			}
+		}
+		return coinl;
+	}
+}
