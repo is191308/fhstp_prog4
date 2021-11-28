fad84c2a231a6aad3abc0f880bd454836711faee
Stefan Berger <is191308@fhstp.ac.at>
2021-11-28 14:55:42 +0100

    Update Prototype

---
+++ src/test/java/fhstp/prog4/coin/CoinTest.java
@@ -0,0 +1,24 @@
+package fhstp.prog4.coin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Executable;
+
+import org.junit.jupiter.api.Test;
+
+import fhstp.prog4.coin.Coin.CoinBuilder;
+
+public class CoinTest {
+
+    @Test
+    public void testConstructor() {
+    	assertTrue(true);
+    }
+    
+    @Test
+    public void newTestCase() {
+        assertTrue(true);
+    }
+    
+    
+}
