826cc9002801939f310291a575ead12ce42ad2d2
Stefan Berger <is191308@fhstp.ac.at>
2021-11-30 18:17:16 +0100

    Formatierung

--- src/main/java/fhstp/prog4/coin/Analyzer.java
+++ src/main/java/fhstp/prog4/coin/Analyzer.java
@@ -13,11 +13,11 @@ public class Analyzer {
 
 	
 	public static void sortByPercentageMove(Set<Coin> coinList) {
-	System.out.println("\nSORTED BY TOP MOVER");
-	coinList.stream()
-	.sorted((c1, c2) -> Double.compare(c2.getPercentage(), c1.getPercentage()))
-	.limit(5)
-	.forEach(c1 -> System.out.println(c1));
+		System.out.println("\nSORTED BY TOP MOVER");
+		coinList.stream()
+		.sorted((c1, c2) -> Double.compare(c2.getPercentage(), c1.getPercentage()))
+		.limit(5)
+		.forEach(c1 -> System.out.println(c1));
 	}
 	
 	
