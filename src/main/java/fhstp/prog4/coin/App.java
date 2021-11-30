69c0d7c399087d29b58831b16df070b57e3692ff
is191320 <is191320@fhstp.ac.at>
2021-11-30 16:35:22 +0100

    new attempt in new Branch

--- src/main/java/fhstp/prog4/coin/App.java
+++ src/main/java/fhstp/prog4/coin/App.java
@@ -1,24 +1,26 @@
 package fhstp.prog4.coin;
 
+
+import java.util.Set;
+import org.json.JSONArray;
+
+
 public class App {
-    
+	
     public static void main(String[] args) {
-    	CoinList cl = new CoinList("ed40901f-9d05-4879-a2a7-b29fd7047d02");
-    	if (cl.updateCoinList()) {
-    		System.out.println("All coins (sorted by price):");
-    		cl.getCoinList().stream()
-			.sorted((c1, c2) -> Double.compare(c2.getPriceUSD(), c1.getPriceUSD()))
-			.forEach(c1 -> System.out.println(c1));
-    		
-    		System.out.println("\nTop movers (24h):");
-    		cl.getCoinList().stream()
-			.sorted((c1, c2) -> Double.compare(c2.getPercentChange24(), c1.getPercentChange24()))
-			.limit(5)
-			.forEach(c1 -> System.out.println(c1));
-    	} else {
-    		System.out.println("Failed to refresh coinList!");
-    	}
+    
+    	//init
+    	API api = new API();
     	
+    	//http & json parse
+    	JSONArray jsonArray = api.receiveJSON();
+    	Set<Coin> coinList = API.convertJSONToCoin(jsonArray);
+    	
+    	//sort
+    	Analyzer.sortByPrice(coinList);
+    	Analyzer.sortByPercentageMove(coinList);
+    	   
     }
+
     
 }
